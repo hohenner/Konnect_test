25f5045e7a9c113f8209391fb31ca53a730f2b26
Andy <hohenner@gmail.com>
2022-06-16 21:33:49 -0700

    testing changes

--- cypress/e2e/konnect.cy.js
+++ cypress/e2e/konnect.cy.js
@@ -28,6 +28,14 @@ describe('access ServiceHub', () => {
     cy.get('#password').type(Cypress.env('PASSWORD'))
     cy.get('button[type=submit]').click()
   })
+
+
+  it('has valid env values', () => {
+    expect(Cypress.env())
+      .to.be.an('object')
+      .and.to.include.keys('USER', 'PASSWORD')
+  })
+
 })
 
 // describe('create new Service', () => {
